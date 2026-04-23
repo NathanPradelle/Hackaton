302bb59c5dac6470fc4f582558b1ac7d54cdb5c2
florian kassi <kassi.florian@gmail.com>
2026-04-23 10:28:18 +0200

    Spring Boot Backend Project

---
+++ back-end/src/test/java/esgi/hackathon/wsd/WsdApplicationTests.java
@@ -0,0 +1,13 @@
+package esgi.hackathon.wsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
